dd5dcc0b99321bb8e31be8b1cabba467726750b5
rkshockey <rkshockey@gmail.com>
2021-06-14 14:32:35 -0400

    Answered question 1.1

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  In counter1, count is inside the function, with another function inside of it, while in counter2 count is global scoped and the function is sinle-layer.
   
   2. Which of the two uses a closure? How can you tell?
   
